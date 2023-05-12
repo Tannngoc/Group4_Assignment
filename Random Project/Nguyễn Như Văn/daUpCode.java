abbc4ad8da18ef9c4b8b1723dd45a5f44d785f3b
NguyenNhuVan <nhuvancmu@gmail.com>
2023-05-12 22:17:42 +0700

    Second Commit

---
+++ Random Project/Nguyễn Như Văn/daUpCode.java
@@ -0,0 +1,11 @@
+
+public class daUpCode {
+
+	public static void main(String[] args) {
+		System.out.println("First Commit");
+		System.out.println("First Commit");
+		System.out.println("First Commit");
+		System.out.println("First Commit");
+	}
+
+}
